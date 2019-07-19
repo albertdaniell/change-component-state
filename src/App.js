2a2f4397bd312a1d54c5effa0fc66724b42c7b33
alby <albertagoya@gmail.com>
2019-07-19 16:34:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Blog from './components/Blog'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    changeContent1 = () => {
+        this.setState({blogContent: "The blog content for component 1 has been changed to this"})
+    }
+
+    changeContent2 = () => {
+        this.setState({blogContent2: "Content for component 2 has been changed"})
+    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            blogContent: "This is the content for the first component",
+            blogContent2: "This is the content for the second component"
+        }
+    }
+    render() {
+        return (
+            <div className="container">
+                <h1>This is a test app
+                </h1>
+                <Blog blogContent={this.state.blogContent}></Blog>
+                <Blog blogContent={this.state.blogContent2}></Blog>
+                <br/>
+                <button className='btn btn-primary' onClick={() => this.changeContent1()}>Change Component 1</button>
+                <br/><br/>
+                <button className='btn btn-primary' onClick={() => this.changeContent2()}>Change Component 2</button>
+
+            </div>
+        );
+    }
 }
 
 export default App;
